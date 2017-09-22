23beadc38a13c2a13805c3b42f3822c0837b0c7f
wk645 <wk645@nyu.edu>
2017-09-22 16:05:10 -0400

    fetch complete

--- app/controllers/api/v1/flights_controller.rb
+++ app/controllers/api/v1/flights_controller.rb
@@ -17,37 +17,20 @@ class Api::V1::FlightsController < ApplicationController
 	def fetchFlight(url)
 		headers = {
         'Authorization' => 'Bearer T1RLAQKQIxO4qNOeiFE/1zDC0BOqHkOETRArl1STWSnakRR6rK/zxZR2AADA5F2CtdzCu1ndeeA+8IZdNJgQwN0CpJ0Rr/du81G/s9BxJmOnRZlub8xq2CyKI34OeEtnhe0VCXzaMC2zOS6Haz+eV4eCKee23cumAOhOtLUQgJ0tiewNezsbk+MVXNXVXszGqqIQ73WhCJr71d37ryWH+5n18kQjOCdYXIQ1bh7gFeqlbsr9pyydAMYPejGSYdwJd778hHkoWWW1RuQyUmm/QCZCPHJHkNivt5exDZ/GOwz9GXsCc4tCW7501LmJ'}
-        debugger
+        # debugger
 		@flight = JSON.parse(RestClient.get(url, headers))["FareInfo"]
 		@flight.map do |f|
 			@flight = Flight.create(
 				rank: f["DestinationRank"],
 				destination: f["DestinationLocation"],
-				airline: f["LowestFare"]["AirlineCodes"].flatten,
-				departureDateTime: f["DepartureDateTime"].split("T")[0],
+				airline: f["LowestFare"]["AirlineCodes"][0],
+				departureDateTime: f["DepartureDateTime"],
 				returnDateTime: f["ReturnDateTime"].split("T")[0],
 				fare: f["LowestFare"]["Fare"]
 			)
 		end
-
 		render json: @flight
 	end
-
-	# def show
-	# 	@flight = JSON.parse(RestClient.get("https://api.test.sabre.com/v2/shop/flights/fares?origin=#{from}&departuredate=#{departDate}&returndate=#{returnDate}&theme=#{theme.toUpperCase()}&maxfare=#{budget}&topdestinations=#{top}"))["fareInfo"]
-	# 	@flight.each do |f|
-	# 		flight = Flight.create(
-	# 			rank: f["DestinationRank"],
-	# 			destination: f["DestinationLocation"],
-	# 			airline: f["LowestFare"]["AirlineCodes"],
-	# 			departureDateTime: f["DepartureDateTime"],
-	# 			returnDateTime: f["ReturnDateTime"],
-	# 			fare: f["LowestFare"]["Fare"]
-	# 		)
-	# 	end
-	# 	render json: @flight
-	# end
-
 
 end
 
