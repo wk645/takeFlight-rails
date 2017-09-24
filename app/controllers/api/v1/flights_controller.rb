40f13ade35d1a00c7d67192e3b2072e52fb03bde
wk645 <wk645@nyu.edu>
2017-09-24 14:21:09 -0400

    backend fetch working

--- app/controllers/api/v1/flights_controller.rb
+++ app/controllers/api/v1/flights_controller.rb
@@ -4,8 +4,8 @@ require 'json'
 class Api::V1::FlightsController < ApplicationController
 
 	def index
-		@flights = Flight.all
-		render json: @flights
+		# @flights = Flight.all
+		# render json: @flight
 	end
 
 	def create
@@ -17,10 +17,10 @@ class Api::V1::FlightsController < ApplicationController
 	def fetchFlight(url)
 		headers = {
         'Authorization' => 'Bearer T1RLAQKQIxO4qNOeiFE/1zDC0BOqHkOETRArl1STWSnakRR6rK/zxZR2AADA5F2CtdzCu1ndeeA+8IZdNJgQwN0CpJ0Rr/du81G/s9BxJmOnRZlub8xq2CyKI34OeEtnhe0VCXzaMC2zOS6Haz+eV4eCKee23cumAOhOtLUQgJ0tiewNezsbk+MVXNXVXszGqqIQ73WhCJr71d37ryWH+5n18kQjOCdYXIQ1bh7gFeqlbsr9pyydAMYPejGSYdwJd778hHkoWWW1RuQyUmm/QCZCPHJHkNivt5exDZ/GOwz9GXsCc4tCW7501LmJ'}
-        # debugger
+        # byebug
 		@flight = JSON.parse(RestClient.get(url, headers))["FareInfo"]
-		@flight.map do |f|
-			@flight = Flight.create(
+		@flight.each do |f|
+			flight = Flight.create(
 				rank: f["DestinationRank"],
 				destination: f["DestinationLocation"],
 				airline: f["LowestFare"]["AirlineCodes"][0],
@@ -29,6 +29,7 @@ class Api::V1::FlightsController < ApplicationController
 				fare: f["LowestFare"]["Fare"]
 			)
 		end
+		# byebug
 		render json: @flight
 	end
 
